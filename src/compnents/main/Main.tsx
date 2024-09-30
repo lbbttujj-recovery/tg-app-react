0fee769af6539316f891e31c0650a09d3a4034b8
lbbttujj <lbbttujj18@gmail.com>
2024-10-01 00:04:52 +0300

    start

--- src/compnents/main/Main.tsx
+++ src/compnents/main/Main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from './Main.module.less'
 export const Main = () => {
+    const tg = window.Telegram.WebApp
     return (
         <div className={styles.main}>
             <h4>Какое настроение сейчас?</h4>
             <p>222</p>
             <div>
-                <button>Выбрать</button>
+                <button onClick={() => tg.close()}>Выбрать</button>
             </div>
         </div>
     )
