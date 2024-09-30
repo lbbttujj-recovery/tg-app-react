c8f7c74efd2e2760a97fc70a542b10f029d7daf0
lbbttujj <lbbttujj18@gmail.com>
2024-09-30 21:35:36 +0300

    start

---
+++ src/compnents/MoodSelect.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from ''
+
+export const MoodSelect = () => {
+    return (
+        <div>
+            <p>222</p>
+            <div>
+                <button>Выбрать</button>
+            </div>
+        </div>
+    )
+}
