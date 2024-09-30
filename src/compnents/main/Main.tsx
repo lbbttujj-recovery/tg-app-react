b8c4df4e9cde36d2ce794d5b92c307314e21a0c9
lbbttujj <lbbttujj18@gmail.com>
2024-10-01 00:52:47 +0300

    start

--- src/compnents/main/Main.tsx
+++ src/compnents/main/Main.tsx
@@ -2,13 +2,13 @@ import React, {useEffect} from "react";
 import styles from './Main.module.less'
 import {useTelegram} from "../../hooks/useTelegram";
 export const Main = () => {
-    const {ready, onClose, onToggleMainButton} = useTelegram()
+    const {ready, onClose, onToggleMainButton, user} = useTelegram()
     useEffect(() => {
         ready()
     },[])
     return (
         <div className={styles.main}>
-            <h4 onClick={onToggleMainButton}>Какое настроение сейчас?</h4>
+            <h4 onClick={onToggleMainButton}>{`Как настроение сейчас, ${user}?`}</h4>
             <p>222</p>
         </div>
     )
