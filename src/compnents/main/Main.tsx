1e717aa253a0e8681b021175bb6f9410892cf783
lbbttujj <lbbttujj18@gmail.com>
2024-10-01 00:42:37 +0300

    start

--- src/compnents/main/Main.tsx
+++ src/compnents/main/Main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from './Main.module.less'
+import {useTelegram} from "../../hooks/useTelegram";
 export const Main = () => {
-    const tg = window.Telegram.WebApp
+    const {ready, onClose, onToggleMainButton} = useTelegram()
+    useEffect(() => {
+        ready()
+    },[])
     return (
         <div className={styles.main}>
-            <h4>Какое настроение сейчас?</h4>
+            <h4 onClick={onToggleMainButton}>Какое настроение сейчас?</h4>
             <p>222</p>
-            <div>
-                <button onClick={() => tg.close()}>Выбрать</button>
-            </div>
         </div>
     )
 }
---
+++ src/hooks/useTelegram.ts
@@ -0,0 +1,26 @@
+const tg = window.Telegram.WebApp
+
+export const useTelegram = () => {
+    const onClose = () => {
+        tg.close()
+    }
+
+    const onToggleMainButton = () => {
+        if(tg.MainButton.isVisible){
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }
+
+    const ready = () => {
+        tg.ready()
+    }
+    return {
+        onClose,
+        ready,
+        onToggleMainButton,
+        tg,
+        user: tg.initDataUnsafe?.user?.username
+    }
+}
