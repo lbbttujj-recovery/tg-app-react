2f011bd8ef0d80e39436385c33627615a01de3e5
lbbttujj <lbbttujj18@gmail.com>
2024-10-02 02:58:36 +0300

    test

--- src/compnents/main/Main.tsx
+++ src/compnents/main/Main.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Main.module.less'
 import { useTelegram } from '../../hooks/useTelegram'
 import { MoodChoice } from '../moodChoice/MoodChoice'
 export const Main = () => {
   const { onToggleMainButton, user } = useTelegram()
+
+  useEffect(() => {
+    fetch('https://lbbttujj.online/test', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }, [])
 
   return (
     <div>
