26ff43551221169cfae3a31a79fee2e8bccff804
lbbttujj <lbbttujj18@gmail.com>
2024-10-02 02:52:25 +0300

    test

--- src/compnents/moodChoice/MoodChoice.tsx
+++ src/compnents/moodChoice/MoodChoice.tsx
@@ -11,7 +11,7 @@ export const MoodChoice = () => {
   const onClickPart = (part: string) => {
     setSelectedSlice(null)
     setIsLoading(true)
-    fetch('http://194.135.105.154:8000/create-image', {
+    fetch('https://lbbttujj.online/create-image', {
       method: 'POST',
       body: JSON.stringify({ mood: part }),
       headers: {
