ee0fba890cc85ef8dda90d3351b71a0f70f2da2a
lbbttujj <lbbttujj18@gmail.com>
2024-10-14 22:55:50 +0300

    add voices

--- src/pages/voice/Voice.tsx
+++ src/pages/voice/Voice.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
 import clsx from 'classnames'
-import { useTelegram } from '../../hooks'
+import { useBackUrl, useTelegram } from '../../hooks'
 import styles from './Voice.module.less'
 import { VoicePlayer } from '../../compnents/voicePlayer/VoicePlayer'
 export const Voice = () => {
   const [voices, setVoices] = useState<string[]>([])
   const { onClose } = useTelegram()
   const [summarizeVoice, setSummarizeVoice] = useState<string>()
-  // const backUrl = useBackUrl()
-  const backUrl = 'https://lbbttujj.online'
+  const [textFromVoice, setTextFromVoice] = useState('')
+  const [brief, setBrief] = useState('')
+  const backUrl = useBackUrl()
+  // const backUrl = 'https://lbbttujj.online'
 
   useEffect(() => {
     axios.get(`${backUrl}/getVoices`).then((res: AxiosResponse<string[]>) => {
@@ -21,9 +23,6 @@ export const Voice = () => {
             const blob = new Blob([audioData], { type: 'audio/mpeg' })
             const audioUrl = URL.createObjectURL(blob) // Создаем URL для аудиофайла
             setVoices((prev) => [...prev, audioUrl])
-            console.log(audioUrl)
-
-            // Теперь можно воспроизводить аудиофайл
           })
           .catch((error) => {
             console.error('Ошибка при получении аудиофайла:', error)
@@ -53,6 +52,18 @@ export const Voice = () => {
     })
   }
 
+  const speechToText = () => {
+    axios.get(`${backUrl}/speechToText`).then((response: AxiosResponse<string>) => {
+      setTextFromVoice(response.data)
+    })
+  }
+
+  const briefHandler = () => {
+    axios.get(`${backUrl}/brief`).then((response: AxiosResponse<string>) => {
+      setBrief(response.data)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.players}>
@@ -60,19 +71,41 @@ export const Voice = () => {
           <VoicePlayer key={voice} src={voice} />
         ))}
       </div>
-      <div className={styles.SummarizePlayer}>
-        <h3>Все вместе</h3>
-        {summarizeVoice ? (
+      {summarizeVoice && (
+        <div className={styles.SummarizePlayer}>
           <VoicePlayer src={summarizeVoice} />
-        ) : (
-          <div className={clsx(styles.Button)} onClick={getSum}>
-            <p>Получить все вместе</p>
-          </div>
-        )}
+        </div>
+      )}
+      {textFromVoice && (
+        <div>
+          <h3>Это в целом:</h3>
+          <p>{textFromVoice}</p>
+        </div>
+      )}
+      {brief && (
+        <div>
+          <h3>Если в кратце: </h3>
+          <p>{brief}</p>
+        </div>
+      )}
+      <div className={clsx(styles.Button, styles.deleteButton)} onClick={getSum}>
+        <div onClick={getSum}>
+          <p>Получить все вместе</p>
+        </div>
       </div>
       <div className={clsx(styles.Button, styles.deleteButton)}>
         <div onClick={deleteHandler}>
           <p>Удалить</p>
+        </div>
+      </div>
+      <div className={clsx(styles.Button, styles.deleteButton)}>
+        <div onClick={speechToText}>
+          <p>Распознать</p>
+        </div>
+      </div>
+      <div className={clsx(styles.Button, styles.deleteButton)}>
+        <div onClick={briefHandler}>
+          <p>Пересказать</p>
         </div>
       </div>
     </div>
